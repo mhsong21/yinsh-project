1ae7d8136d699211e8d5a63773c4f1b036fe9650
mhsong <sniaw21@gmail.com>
2017-12-13 20:04:27 +0900

    Button Test Improved

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -8,12 +8,27 @@ public class ButtonClick : MonoBehaviour
 	public MapManager parent;
 	public Animator animator;
 
+	private bool onEnter = false;
 	private bool onDown = false;
 
-	public void OnMouseDown()
+	public void OnMouseEnter()
 	{
 		// parent.OnChildClick(gameObject);
 		animator.SetBool("Pointer Down", true);
+		onEnter = true;
+	}
+
+	public void OnMouseExit()
+	{
+		if (onEnter)
+		{
+			animator.SetBool("Pointer Down", false);
+			onEnter = false;
+		}
+	}
+
+	public void OnMouseDown()
+	{
 		onDown = true;
 	}
 
@@ -21,7 +36,9 @@ public class ButtonClick : MonoBehaviour
 	{
 		if (onDown)
 		{
-			animator.SetBool("Pointer Down", false);
+			var renderer = gameObject.GetComponent<MeshRenderer>();
+			Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+			renderer.materials[0].color = color;
 			onDown = false;
 		}
 	}
